038547e90fcff2fcefd086a91cb7ed350f2c9391
jsweetpotato37 <jsweetpotato37@gmail.com>
2024-07-25 06:21:16 +0900

    섹션1 - 12(해설) 섹션4 - 1(문제풀이)

--- 문제풀이/timeCheck.js
+++ 문제풀이/timeCheck.js
@@ -1,11 +1,14 @@
 export function timeCheck(func, ...args) {
   const start = performance.now(); // 측정 시작
   const start1 = new Date(); // 측정 시작
+
+  console.time("계산시간");
   const answer = func(...args);
+  console.timeEnd("계산시간");
   const end = performance.now(); // 측정 종료
 
   const end1 = new Date(); // 측정 시작
 
   const time = ` ${end - start}ms`;
-  return [answer, time];
+  return answer;
 }
