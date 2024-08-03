a700799f8166c5ff677112c3f8566b6102b9f807
jsweetpotato37 <jsweetpotato37@gmail.com>
2024-08-04 00:00:20 +0900

    스타일 변경, 오류 수정

--- 문제풀이/timeCheck.js
+++ 문제풀이/timeCheck.js
@@ -1,14 +1,44 @@
-export function timeCheck(func, ...args) {
-  const start = performance.now(); // 측정 시작
-  const start1 = new Date(); // 측정 시작
+const typeCheck = (target) => {
+  return Object.prototype.toString.call(target).slice(8, -1);
+};
 
+export function timeCheck(func, ...args) {
   console.time("계산시간");
   const answer = func(...args);
   console.timeEnd("계산시간");
-  const end = performance.now(); // 측정 종료
+  let renderArg = "";
+  [...args].forEach((v) => {
+    const type = typeCheck(v);
+    switch (type) {
+      case "Array":
+        let text = "";
+        if (Array.isArray(v[0]))
+          v.every((item) => {
+            return (text += `[${item}]${v.at(-1) === item ? "" : ", <br>"}`);
+          });
+        else text = v;
+        renderArg += `[${text}]`;
+        break;
 
-  const end1 = new Date(); // 측정 시작
+      default:
+        renderArg += `${v} <br />`;
+        break;
+    }
+  });
 
-  const time = ` ${end - start}ms`;
-  return answer;
+  return [renderArg, answer];
+}
+
+export function render(answer, answerBox) {
+  answer.map((a) =>
+    answerBox.insertAdjacentHTML(
+      "beforeend",
+      ` <div id="answer">
+          <h3>입력</h3>
+          <p>${a[0]}</p>
+          <h3>출력</h3>
+          <p>${a[1]}</p>
+        </div>`
+    )
+  );
 }
